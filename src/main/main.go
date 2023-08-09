a8c58e27ebad2c320a9a57dedb926e97d7fb0270
capDoYeonLee <ghtyu21@naver.com>
2023-08-09 15:10:30 +0900

    feat: reading writing json

--- src/main/main.go
+++ src/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,13 +11,25 @@ func main() {
 
 	port := 8080
 
-	http.HandleFunc("/helloworld", helloworldHandler)
+	http.HandleFunc("/helloworld", helloWorldHandler)
 
 	log.Printf("Server starting on port %v\n ", 8080)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 
 }
 
-func helloworldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world\n")
+type helloWorldResponse struct {
+	Message string
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprint(w, "Hello world\n")
+	response := helloWorldResponse{Message: "HelloWorld"}
+	data, err := json.Marshal(response)
+	if err != nil {
+		panic("Ooops")
+	}
+
+	fmt.Fprint(w, string(data))
+
 }
